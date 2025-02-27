7d0c307a9617b7aa97f1eafb92714abc834dee80
Villegas-Morral <131444971+villegas-morral@users.noreply.github.com>
2025-02-27 12:44:17 +0100

    added ks to full swarming

--- code_opinion-dynamics/full-swarming.py
+++ code_opinion-dynamics/full-swarming.py
@@ -9,7 +9,7 @@ def nabla_morse_potential(r, c_rep, c_att, l_rep, l_att):
 
 
 def ode_system(x_step, v_step, w_step, n_l, n_f, n_u, 
-               alpha, beta, nabla_u, r_x, r_w, gammas_blue, gammas_red, tau_blue_l, tau_red_f):  
+               alpha, beta, nabla_u, r_x, r_w, gammas_blue, gammas_red, tau_blue_l, tau_red_f, ks):  
     xs = [
         x_step[:n_l],
         x_step[n_l : n_l+n_f],
@@ -34,21 +34,21 @@ def ode_system(x_step, v_step, w_step, n_l, n_f, n_u,
     for i in range(3):
         term_1 = (alpha - beta * np.sum(vs[i] ** 2, axis=1, keepdims=True)) * vs[i]
 
-        term_2 = sum(potential_sum(xs[i], xs[j], nabla_u) / ns[j] for j in range(3))
+        term_2 = sum(ks[i,j] * potential_sum(xs[i], xs[j], nabla_u) / ns[j] for j in range(3))
         
         term_3 = gammas_blue[i] * velocity_alignment(vs[i], v_blue, ws[i], w_blue, r_w) \
                     + gammas_red[i] * velocity_alignment(vs[i], v_red, ws[i], w_red, r_w)
                     
         dvs[i] = term_1 + term_2 + term_3
 
-    phis = [[None for _ in range(3)] for _ in range(3)]
+    phis = np.empty((3, 3), dtype=object)
     for i in range(3):
         for j in range(3):
-            phis[i][j] = opinion_alignment_sum(xs[i], xs[j], ws[i], ws[j], r_x, r_w) / ns[j]
+            phis[i,j] = ks[i,j] * opinion_alignment_sum(xs[i], xs[j], ws[i], ws[j], r_x, r_w) / ns[j]
 
-    dw_l = phis[0][0] + phis[0][1] + 0*phis[0][2] - tau_blue_l * (ws[0]-w_blue)
-    dw_f = phis[1][1] + phis[1][0] + phis[1][2] - tau_red_f * (ws[1]-w_red)
-    dw_u = 0*phis[2][2] + 0*phis[2][1] + 0*phis[2][0]
+    dw_l = phis[0,0] + phis[0,1] + 0*phis[0,2] - tau_blue_l * (ws[0]-w_blue)
+    dw_f = phis[1,1] + phis[1,0] + phis[1,2] - tau_red_f * (ws[1]-w_red)
+    dw_u = 0*phis[2,2] + 0*phis[2,1] + 0*phis[2,0]
     
     dx = np.vstack(vs)
     dv = np.vstack(dvs)
@@ -103,7 +103,8 @@ def opinion_alignment_sum(x1, x2, w1, w2, r_x, r_w):
 
 n_l = 20
 n_f = 50
-n_u = 50
+# n_u = 50
+n_u = 0
 n = n_l + n_f + n_u
 
 t_final = 100
@@ -113,6 +114,7 @@ steps = int(np.floor(t_final / dt))
 x = np.zeros((steps, n, 2))
 v = np.zeros((steps, n, 2))
 w = np.zeros((steps, n))
+if n_u == 0: n_u = 1
 
 r_x = 1
 r_w = 0.5
@@ -120,8 +122,17 @@ alpha = 1
 beta = 0.5
 gammas_red = [1, 1, 0]
 gammas_blue = [1, 1, 0]
-tau_blue_l = 0.1
-tau_red_f = 0.01
+
+ks = np.array([
+    [1, 1, 1],  # ll lf lu
+    [1, 1, 1],  # fl ff fu
+    [1, 1, 1]   # ul uf uu
+])
+
+tau_blue_l = 0
+tau_red_f = 0
+# tau_blue_l = 0.1
+# tau_red_f = 0.01
 
 c_att, l_att = 50, 1
 c_rep, l_rep = 60, 0.5
@@ -130,6 +141,7 @@ nabla_u = lambda r: nabla_morse_potential(r, c_rep, c_att, l_rep, l_att)
 
 # INTEGRATION STEP
 
+# np.random.seed(4321)
 np.random.seed(1234)
 
 # [VVM] why is this the domain
@@ -142,7 +154,7 @@ w[0] = np.hstack([
 
 # Preparation: compute the first solution using Euler. Use it to compute the second system.
 
-dx_0, dv_0, dw_0 = ode_system(x[0], v[0], w[0], n_l, n_f, n_u, alpha, beta, nabla_u, r_x, r_w, gammas_blue, gammas_red, tau_blue_l, tau_red_f)
+dx_0, dv_0, dw_0 = ode_system(x[0], v[0], w[0], n_l, n_f, n_u, alpha, beta, nabla_u, r_x, r_w, gammas_blue, gammas_red, tau_blue_l, tau_red_f, ks)
 x[1] = x[0] + dt*dx_0
 v[1] = v[0] + dt*dv_0
 w[1] = w[0] + dt*dw_0
@@ -150,7 +162,7 @@ w[1] = w[0] + dt*dw_0
 # Adams-Bashforth 2-step method
 
 for i in range(1, steps-1):
-    dx_1, dv_1, dw_1 = ode_system(x[i], v[i], w[i], n_l, n_f, n_u, alpha, beta, nabla_u, r_x, r_w, gammas_blue, gammas_red, tau_blue_l, tau_red_f)
+    dx_1, dv_1, dw_1 = ode_system(x[i], v[i], w[i], n_l, n_f, n_u, alpha, beta, nabla_u, r_x, r_w, gammas_blue, gammas_red, tau_blue_l, tau_red_f, ks)
     
     x[i+1] = x[i] + (dt / 2) * (3 * dx_1 - dx_0)
     v[i+1] = v[i] + (dt / 2) * (3 * dv_1 - dv_0)
@@ -171,7 +183,7 @@ plt.figure()
 plt.plot(x[-1, :n_l, 0], x[-1, :n_l, 1], 'o', c='b')
 plt.plot(x[-1, n_l:n_l+n_f, 0], x[-1, n_l:n_l+n_f, 1], 'o', c='r')
 plt.plot(x[-1, n_l+n_f:n, 0], x[-1, n_l+n_f:n, 1], 'o', c='k')
-plt.quiver(x[-1, :, 0], x[-1, :, 1], v[-1, :, 0], v[-1, :, 1], color='r')
+plt.quiver(x[-1, :, 0], x[-1, :, 1], v[-1, :, 0], v[-1, :, 1], color='k')
 plt.xlabel('x', fontsize=14)
 plt.ylabel('y', fontsize=14)
 plt.xticks(fontsize=12)
@@ -180,10 +192,29 @@ plt.grid(False)
 plt.title('Velocities at the final time', fontsize=16, fontweight='bold')
 plt.axis('equal')
 
-output_file = os.path.join(output_folder, 'final_velocity_configuration.svg')
+output_file = os.path.join(output_folder, 'velocity.svg')
 plt.savefig(output_file)
 
-plt.show()
+# plt.show()
+
+
+# PLOT: opinion over time
+
+plt.figure()
+plt.plot(range(steps), w[:, :n_l], 'b', linewidth=1.5)
+plt.plot(range(steps), w[:, n_l:n_l+n_f], 'r', linewidth=1.5)
+plt.plot(range(steps), w[:, n_l+n_f:], 'k', linewidth=1.5)
+plt.xlabel('Step', fontsize=14)
+plt.ylabel('Opinion', fontsize=14)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.title('Opinion Over Time')
+plt.grid(False)
+
+output_file = os.path.join(output_folder, 'opinion.svg')
+plt.savefig(output_file)
+
+# plt.show()
 
 
 # # PLOT: velocities over time
@@ -225,25 +256,6 @@ plt.show()
 # plt.savefig(output_file)
 
 # plt.show()
-
-
-# PLOT: opinion over time
-
-plt.figure()
-plt.plot(range(steps), w[:, :n_l], 'b', linewidth=1.5)
-plt.plot(range(steps), w[:, n_l:n_l+n_f], 'r', linewidth=1.5)
-plt.plot(range(steps), w[:, n_l+n_f:], 'k', linewidth=1.5)
-plt.xlabel('Step', fontsize=14)
-plt.ylabel('Opinion', fontsize=14)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.title('Opinion Over Time')
-plt.grid(False)
-
-output_file = os.path.join(output_folder, 'opinion.svg')
-plt.savefig(output_file)
-
-plt.show()
 
 
 # # PLOT: mean velocities over time
@@ -304,4 +316,3 @@ plt.show()
 # plt.savefig(output_file)
 
 # plt.show()
-
